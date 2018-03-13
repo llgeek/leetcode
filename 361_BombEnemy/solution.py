7e36efe384ac8b4ab0c9541e6d89908ecefd04da
llgeek <angleflycll@gmail.com>
2018-03-13 18:14:58 -0500

    solve more problems, tagged Google

---
+++ 361_BombEnemy/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def bombEnemy(self, grid):
+        """ 
+        type grid: list of list, 'E' represents enemy, 'W' represents wall, 0 represents empty cell
+        trype: int, number of enemies you can kill
+        """
+        
