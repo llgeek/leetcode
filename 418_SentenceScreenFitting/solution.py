41596c37db61442cec1a7d8f02214e0b22e6c4cb
llgeek <angleflycll@gmail.com>
2018-03-12 21:13:22 -0500

    solve more problems, tagged as Google

---
+++ 418_SentenceScreenFitting/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sentenceFitting(self, sentence, rows, cols):
+        """
+        type sentence: list of words
+        type rows: int
+        type cols: int
+        rtype: int
+        """
+
+        
+        
